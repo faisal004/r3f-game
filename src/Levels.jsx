a8bf706aee926ae36c58b0b96682739d10c7474d
Faisal Husain <faisalhusain1320@gmail.com>
2025-05-05 22:34:45 +0530

    add : 3rd trap done

--- src/Levels.jsx
+++ src/Levels.jsx
@@ -36,12 +36,12 @@ function BlockSpinner({ position = [0, 0, 0] }) {
 }
 function BlockLimbo({ position = [0, 0, 0] }) {
     const obstacle = useRef()
-    const [timeOffSet] = useState(() => Math.random() * Math.PI *2)
+    const [timeOffSet] = useState(() => Math.random() * Math.PI * 2)
     useFrame((state) => {
         const time = state.clock.getElapsedTime()
 
         const y = Math.sin(time + timeOffSet) + 1.15
-        obstacle.current.setNextKinematicTranslation({ x: position[0],y: position[1] + y, z: position[2] })
+        obstacle.current.setNextKinematicTranslation({ x: position[0], y: position[1] + y, z: position[2] })
     })
     return <group position={position}>
         <mesh geometry={boxGeometry} material={floor2Material} position={[0, - 0.1, 0]} scale={[4, 0.2, 4]} receiveShadow />
@@ -52,31 +52,30 @@ function BlockLimbo({ position = [0, 0, 0] }) {
     </group>
 
 }
-function BlockAxe({ position = [ 0, 0, 0 ] })
-{
+function BlockAxe({ position = [0, 0, 0] }) {
     const obstacle = useRef()
-    const [ timeOffset ] = useState(() => Math.random() * Math.PI * 2)
+    const [timeOffset] = useState(() => Math.random() * Math.PI * 2)
 
-    useFrame((state) =>
-        {
-            const time = state.clock.getElapsedTime()
-        
-            const x = Math.sin(time + timeOffset) * 1.25
-                        obstacle.current.setNextKinematicTranslation({ x: position[0] + x, y: position[1] + 0.75, z: position[2] })        })
+    useFrame((state) => {
+        const time = state.clock.getElapsedTime()
 
-    return <group position={ position }>
-        <mesh geometry={ boxGeometry } material={ floor2Material } position={ [ 0, - 0.1, 0 ] } scale={ [ 4, 0.2, 4 ] }  receiveShadow />
-        <RigidBody ref={ obstacle } type="kinematicPosition" position={ [ 0, 0.3, 0 ] } restitution={ 0.2 } friction={ 0 }>
-        <mesh geometry={ boxGeometry } material={ obstacleMaterial } scale={ [ 1.5, 1.5, 0.3 ] } castShadow receiveShadow />        </RigidBody>
+        const x = Math.sin(time + timeOffset) * 1.25
+        obstacle.current.setNextKinematicTranslation({ x: position[0] + x, y: position[1] + 0.75, z: position[2] })
+    })
+
+    return <group position={position}>
+        <mesh geometry={boxGeometry} material={floor2Material} position={[0, - 0.1, 0]} scale={[4, 0.2, 4]} receiveShadow />
+        <RigidBody ref={obstacle} type="kinematicPosition" position={[0, 0.3, 0]} restitution={0.2} friction={0}>
+            <mesh geometry={boxGeometry} material={obstacleMaterial} scale={[1.5, 1.5, 0.3]} castShadow receiveShadow />        </RigidBody>
     </group>
 }
 const Levels = () => {
     return <>
 
-<BlockStart position={ [ 0, 0, 12 ] } />
-        <BlockSpinner position={ [ 0, 0, 8 ] } />
-        <BlockLimbo position={ [ 0, 0, 4 ] } />
-        <BlockAxe position={ [ 0, 0, 0 ] } />
+        <BlockStart position={[0, 0, 12]} />
+        <BlockSpinner position={[0, 0, 8]} />
+        <BlockLimbo position={[0, 0, 4]} />
+        <BlockAxe position={[0, 0, 0]} />
 
 
     </>
