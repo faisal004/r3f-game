30b0954a9375f02e6f44e81390a9d07ba9214428
Faisal Husain <faisalhusain1320@gmail.com>
2025-05-05 22:34:38 +0530

    add : 3rd trap done

--- src/Levels.jsx
+++ src/Levels.jsx
@@ -52,12 +52,31 @@ function BlockLimbo({ position = [0, 0, 0] }) {
     </group>
 
 }
+function BlockAxe({ position = [ 0, 0, 0 ] })
+{
+    const obstacle = useRef()
+    const [ timeOffset ] = useState(() => Math.random() * Math.PI * 2)
+
+    useFrame((state) =>
+        {
+            const time = state.clock.getElapsedTime()
+        
+            const x = Math.sin(time + timeOffset) * 1.25
+                        obstacle.current.setNextKinematicTranslation({ x: position[0] + x, y: position[1] + 0.75, z: position[2] })        })
+
+    return <group position={ position }>
+        <mesh geometry={ boxGeometry } material={ floor2Material } position={ [ 0, - 0.1, 0 ] } scale={ [ 4, 0.2, 4 ] }  receiveShadow />
+        <RigidBody ref={ obstacle } type="kinematicPosition" position={ [ 0, 0.3, 0 ] } restitution={ 0.2 } friction={ 0 }>
+        <mesh geometry={ boxGeometry } material={ obstacleMaterial } scale={ [ 1.5, 1.5, 0.3 ] } castShadow receiveShadow />        </RigidBody>
+    </group>
+}
 const Levels = () => {
     return <>
 
-        <BlockStart position={[0, 0, 8]} />
-        <BlockSpinner position={[0, 0, 4]} />
-        <BlockLimbo position={[0, 0, 0]} />
+<BlockStart position={ [ 0, 0, 12 ] } />
+        <BlockSpinner position={ [ 0, 0, 8 ] } />
+        <BlockLimbo position={ [ 0, 0, 4 ] } />
+        <BlockAxe position={ [ 0, 0, 0 ] } />
 
 
     </>
