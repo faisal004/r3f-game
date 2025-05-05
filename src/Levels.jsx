e2c3e8857c30cffb5f7c064a834c16a1f5fd5e7d
Faisal Husain <faisalhusain1320@gmail.com>
2025-05-05 22:30:15 +0530

    add : 2nd trap done

--- src/Levels.jsx
+++ src/Levels.jsx
@@ -17,30 +17,47 @@ function BlockStart({ position = [0, 0, 0] }) {
 }
 function BlockSpinner({ position = [0, 0, 0] }) {
     const obstacle = useRef()
-    const [ speed ] = useState(() => (Math.random() + 0.2) * (Math.random() < 0.5 ? - 1 : 1))
-    useFrame((state) =>
-        {
-            const time = state.clock.getElapsedTime()
-        
-            const rotation = new THREE.Quaternion()
-            rotation.setFromEuler(new THREE.Euler(0, time * speed, 0))
-                        obstacle.current.setNextKinematicRotation(rotation)
-        })
+    const [speed] = useState(() => (Math.random() + 0.2) * (Math.random() < 0.5 ? - 1 : 1))
+    useFrame((state) => {
+        const time = state.clock.getElapsedTime()
+
+        const rotation = new THREE.Quaternion()
+        rotation.setFromEuler(new THREE.Euler(0, time * speed, 0))
+        obstacle.current.setNextKinematicRotation(rotation)
+    })
     return <group position={position}>
         <mesh geometry={boxGeometry} material={floor2Material} position={[0, - 0.1, 0]} scale={[4, 0.2, 4]} receiveShadow />
-        <RigidBody ref={ obstacle } type='kinematicPosition' position={[0,0.3,0]} restitution={0.2} friction={0.2}>
-        <mesh geometry={boxGeometry} material={obstacleMaterial} scale={[3.5, 0.3, 0.3]} castShadow receiveShadow />
+        <RigidBody ref={obstacle} type='kinematicPosition' position={[0, 0.3, 0]} restitution={0.2} friction={0.2}>
+            <mesh geometry={boxGeometry} material={obstacleMaterial} scale={[3.5, 0.3, 0.3]} castShadow receiveShadow />
 
         </RigidBody>
     </group>
 
 }
+function BlockLimbo({ position = [0, 0, 0] }) {
+    const obstacle = useRef()
+    const [timeOffSet] = useState(() => Math.random() * Math.PI *2)
+    useFrame((state) => {
+        const time = state.clock.getElapsedTime()
 
+        const y = Math.sin(time + timeOffSet) + 1.15
+        obstacle.current.setNextKinematicTranslation({ x: position[0],y: position[1] + y, z: position[2] })
+    })
+    return <group position={position}>
+        <mesh geometry={boxGeometry} material={floor2Material} position={[0, - 0.1, 0]} scale={[4, 0.2, 4]} receiveShadow />
+        <RigidBody ref={obstacle} type='kinematicPosition' position={[0, 0.3, 0]} restitution={0.2} friction={0.2}>
+            <mesh geometry={boxGeometry} material={obstacleMaterial} scale={[3.5, 0.3, 0.3]} castShadow receiveShadow />
+
+        </RigidBody>
+    </group>
+
+}
 const Levels = () => {
     return <>
 
-        <BlockStart position={[0, 0, 4]} />
-        <BlockSpinner position={[0, 0, 0]} />
+        <BlockStart position={[0, 0, 8]} />
+        <BlockSpinner position={[0, 0, 4]} />
+        <BlockLimbo position={[0, 0, 0]} />
 
 
     </>
