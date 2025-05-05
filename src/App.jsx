76def7f8be342b3b098584ab8dbebc53e064fef8
Faisal Husain <faisalhusain1320@gmail.com>
2025-05-05 20:14:02 +0530

    new

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+// App.js
+import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import Experience from './Experience.jsx';
+
+function App() {
+  return (
+    <Canvas
+    shadows
+    camera={ {
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [ 2.5, 4, 6 ]
+    } }
+>
+    <Experience />
+</Canvas>
+  );
+}
+
+export default App;
