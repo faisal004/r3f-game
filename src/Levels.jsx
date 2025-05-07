3e415caf3b46945223c73fdb3c28524002c4db0e
Faisal Husain <faisalhusain1320@gmail.com>
2025-05-07 19:17:03 +0530

    add :  continueing with this

--- src/Levels.jsx
+++ src/Levels.jsx
@@ -2,11 +2,12 @@ import { RigidBody } from '@react-three/rapier'
 import * as THREE from 'three'
 import { useState, useRef } from 'react'
 import { useFrame } from '@react-three/fiber'
+import {useGLTF} from '@react-three/drei'
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
 const floor1Material = new THREE.MeshStandardMaterial({ color: 'limegreen' })
 const floor2Material = new THREE.MeshStandardMaterial({ color: 'greenyellow' })
 const obstacleMaterial = new THREE.MeshStandardMaterial({ color: 'orangered' })
-const wallMaterial = new THREE.MeshStandardMaterial({ color: 'slategrey' })
+// const wallMaterial = new THREE.MeshStandardMaterial({ color: 'slategrey' })
 
 function BlockStart({ position = [0, 0, 0] }) {
     return <group position={position}>
@@ -16,9 +17,11 @@ function BlockStart({ position = [0, 0, 0] }) {
 
 }
 function BlockEnd({ position = [0, 0, 0] }) {
+
+    const trophy = useGLTF('/model.gltf')
     return <group position={position}>
         <mesh geometry={boxGeometry} material={floor1Material} position={[0, 0, 0]} scale={[4, 0.2, 4]} receiveShadow />
-
+        <primitive object={trophy.scene}  scale={0.6}/>
     </group>
 
 }
