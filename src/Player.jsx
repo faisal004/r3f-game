afcaaa3f12cabdbf6ae445b64e58d9bb5701b0bd
Faisal Husain <faisalhusain1320@gmail.com>
2025-05-25 21:51:03 +0530

    add : jump done

--- src/Player.jsx
+++ src/Player.jsx
@@ -1,28 +1,40 @@
-import { RigidBody } from "@react-three/rapier"
+import { RigidBody, useRapier } from "@react-three/rapier"
 import { useFrame } from '@react-three/fiber'
 import { useKeyboardControls } from '@react-three/drei'
-import { useRef ,useEffect } from "react"
+import { useRef, useEffect } from "react"
+
 const Players = () => {
     const [subscribeKeys, getKeys] = useKeyboardControls()
     const body = useRef()
+
+    const { rapier, world } = useRapier()
+    const rapierWorld = world
     const jump = () => {
         const origin = body.current.translation()
         origin.y -= 0.31
-        body.current.applyImpulse({ x: 0, y: 0.5, z: 0 })
-    }
-useEffect(() => {
-    subscribeKeys(
-        (state)=>{
-return state.jump
-        },
-        (value)=>{
-            if(value){
-jump()
-            }
+        const direction = { x: 0, y: -1, z: 0 }
+        const ray = new rapier.Ray(origin, direction)
+        const hit = rapierWorld.castRay(ray, 10, true)
+        if (hit.timeOfImpact < 0.15) {
+            body.current.applyImpulse({ x: 0, y: 0.5, z: 0 })
+
         }
-    )
-    
-},[])
+    }
+    useEffect(() => {
+        const jumpSubscription = subscribeKeys(
+            (state) => {
+                return state.jump
+            },
+            (value) => {
+                if (value) {
+                    jump()
+                }
+            }
+        )
+        return () => {
+            jumpSubscription()
+        }
+    }, [])
     useFrame((state, delta) => {
         const { forward, backward, leftward, rightward } = getKeys()
 
@@ -54,10 +66,10 @@ jump()
     })
     return (
         <>
-            <RigidBody 
-            linearDamping={0.5}
-            angularDamping={0.5}
-            ref={body} canSleep={false} colliders='ball' restitution={0.2} friction={1} position={[0, 1, 0]}>
+            <RigidBody
+                linearDamping={0.5}
+                angularDamping={0.5}
+                ref={body} canSleep={false} colliders='ball' restitution={0.2} friction={1} position={[0, 1, 0]}>
                 <mesh castShadow>
                     <icosahedronGeometry args={[0.3, 1]} />
                     <meshStandardMaterial flatShading color='hotpink' />
